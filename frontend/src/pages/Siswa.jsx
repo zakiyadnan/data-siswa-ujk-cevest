84496fa4345129f11ce8c00fae34bdbbe8770046
ZakiyAdnanMahendra <zakiyadnan1411@gmail.com>
2025-12-27 13:43:05 +0700

    fix: perbaiki format tampilan tanggal siswa

--- frontend/src/pages/Siswa.jsx
+++ frontend/src/pages/Siswa.jsx
@@ -6,26 +6,33 @@ import api from "../services/api";
 //Mengambil koneksi API (Axios) ke backend.
 import SiswaForm from "../components/SiswaForm";
 //Mengambil komponen Form input siswa.
+
 const Siswa = () => {
   //Membuat halaman utama data siswa
+
   const [siswa, setSiswa] = useState([]);
   //Menyimpan semua data siswa dari database.
+
   const [editData, setEditData] = useState(null);
   //Menyimpan data siswa yang sedang diedit.
+
   const getSiswa = async () => {
     const res = await api.get("/siswa");
     setSiswa(res.data);
   };
   //Mengambil semua data siswa dari backend lalu disimpan ke state
+
   const deleteSiswa = async (kode) => {
     await api.delete(`/siswa/${kode}`);
     getSiswa();
   };
   //Menghapus data siswa berdasarkan kode lalu refresh tabel
+
   useEffect(() => {
     getSiswa();
   }, []);
   //Saat halaman pertama kali dibuka, data otomatis dimuat.
+
   return (
     <>
       <SiswaForm
@@ -51,7 +58,12 @@ const Siswa = () => {
               <td>{s.kode_siswa}</td>
               <td>{s.nama_siswa}</td>
               <td>{s.alamat_siswa}</td>
-              <td>{s.tgl_siswa}</td>
+
+              {/* Perbaikan format tanggal agar tidak tampil ISO */}
+              <td>
+                {new Date(s.tgl_siswa).toLocaleDateString("id-ID")}
+              </td>
+
               <td>{s.jurusan_siswa}</td>
               <td>
                 <button
